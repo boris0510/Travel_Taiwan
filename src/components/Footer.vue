d8dbd208e7f6775da75e7cfbb88198dbb51ce275
Boris Huang <scps880510@gmail.com>
2021-11-06 22:08:33 +0800

    撰寫footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer class="footer">
+    <p>CopyRight Travel Taiwan , Design By Tracy , Code By Boris</p>
+  </footer>
+</template>
+
+<style scoped>
+.footer{
+  max-width: 100%;
+  height: 120px;
+  background: #08A6BB;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.footer p{
+  font-weight: bold;
+  font-size: 20px;
+  color: #FFFFFF;
+  text-align: center;
+  margin: 0;
+}
+</style>
