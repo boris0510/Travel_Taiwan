1c8e5871f9ca302799ba1f5b7efeaf33047f1112
Boris Huang <scps880510@gmail.com>
2021-11-08 14:30:01 +0800

    新增mitt

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import { createApp } from 'vue';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
-import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
 
 import App from './App.vue';
 import router from './router';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+import emitter from './tools/mitt';
 
 const app = createApp(App);
 app.use(VueAxios, axios);
+app.config.globalProperties.emitter = emitter;
 app.use(router);
 app.mount('#app');
