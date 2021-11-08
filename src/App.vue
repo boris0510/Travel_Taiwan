5ee33cac4de5ca18f255e1298496f7bb07cbb22a
Boris Huang <scps880510@gmail.com>
2021-11-08 21:36:21 +0800

    修改

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,26 @@
 <template>
+  <GoTop />
+  <Navber />
+  <Banner />
   <Router-view/>
+  <Footer />
 </template>
+<script>
+import Navber from '@/components/Navber.vue';
+import Banner from '@/components/Banner.vue';
+import Footer from '@/components/Footer.vue';
+import GoTop from '@/components/GoTop.vue';
+
+export default {
+  name: 'Attractions',
+  components: {
+    Navber,
+    Banner,
+    Footer,
+    GoTop,
+  },
+};
+</script>
 
 <style lang="scss">
 @import './assets/scss/all.scss';
